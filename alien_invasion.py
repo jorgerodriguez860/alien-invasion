4a7653b3bec1ab052ec0f9ccbefa882d3d2385f0
Jorge Rodriguez <rjorgeluis16@gmail.com>
2023-01-23 16:26:22 -0500

    detected and responded to alien and ship collisions

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,10 @@
 import sys
+from time import sleep
+
 import pygame
+
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -22,11 +26,13 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height)
         )
-
         pygame.display.set_caption("Alien Invasion")
 
         # Set the background color.
         self.bg_color = self.settings.bg_color
+
+        # Create an instance to store game statistics.
+        self.stats = GameStats(self)
 
         # Instantiate game assets.
         self.ship = Ship(self)
@@ -111,6 +117,26 @@ class AlienInvasion:
         """
         self._check_fleet_edges()
         self.aliens.update()
+
+        # Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            self._ship_hit()
+
+    def _ship_hit(self):
+        """Respond to the ship being hit by an alien."""
+        # Decrement ships_left.
+        self.stats.ships_left -= 1
+
+        # Get rid of any remaining aliens and bullets.
+        self.aliens.empty()
+        self.bullets.empty()
+
+        # Create a new fleet and center the ship.
+        self._create_fleet()
+        self.ship.center_ship()
+
+        # Pause.
+        sleep(0.5)
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
